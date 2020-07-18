b814a25d3f1aba1cbd7c92974ff9036a03121bcd
Wald Schlafer <aural.student@gmail.com>
2020-07-18 20:17:28 +0530

    Got it working ! ffmpeg in Swift !!!

--- CAndSwift/CAndSwift/AppDelegate.swift
+++ CAndSwift/CAndSwift/AppDelegate.swift
@@ -18,9 +18,26 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to initialize your application
         av_register_all()
         avfilter_register_all()
-        avformat_network_init()
-        let formatContext = avformat_alloc_context()
-        print(formatContext)
+        var formatContext = avformat_alloc_context()
+
+        // Open the file and read its header. The codecs are not opened.
+        // The function arguments are:
+        // AVFormatContext (the component we allocated memory for),
+        // url (filename),
+        // AVInputFormat (if you pass NULL it'll do the auto detect)
+        // and AVDictionary (which are options to the demuxer)
+        // http://ffmpeg.org/doxygen/trunk/group__lavf__decoding.html#ga31d601155e9035d5b0e7efedc894ee49
+        
+        let url = URL(fileURLWithPath: "/Volumes/MyData/Music/Bourne.wav")
+        
+        let err = avformat_open_input(&formatContext, "/Volumes/MyData/Music/Bourne.wav", nil, nil)
+        if err == 0 {
+            
+            let e2 = avformat_find_stream_info(formatContext, nil)
+            if e2 == 0 {
+                print("Duration is:", formatContext?.pointee.duration)
+            }
+        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
--- CAndSwift/CAndSwift/ffmpeg.h
+++ CAndSwift/CAndSwift/ffmpeg.h
@@ -3,6 +3,10 @@
 
 #import <libavcodec/avcodec.h>
 #import <libavformat/avformat.h>
+#import <libavutil/avutil.h>
 #import <libavfilter/avfilter.h>
+#import <libavresample/avresample.h>
+#import <libswscale/swscale.h>
+
 
 #endif /* ffmpeg_h */
