1ea0424977fafb33f1e6e849ff25e97b23e3374a
Wald Schlafer <aural.student@gmail.com>
2020-08-07 12:02:53 +0530

    Added all audio decoders to ffmpeg

--- FFmpegPlayer/Resources/ffmpeg/build-ffmpeg.sh
+++ FFmpegPlayer/Resources/ffmpeg/build-ffmpeg.sh
@@ -24,11 +24,6 @@ cd $sourceDirectoryName
 pwd
 echo "Configuring FFmpeg ..."
 
-#--pkg-config-flags="--shared"
-#--extra-ldexeflags="-Bshared -mmacosx-version-min=10.12"
-#--extra-ldflags="-Bshared -mmacosx-version-min=10.12"
-#--extra-cflags="-Bshared -mmacosx-version-min=10.12"
-
 ./configure \
 --cc=/usr/bin/clang \
 --extra-ldexeflags="-mmacosx-version-min=10.12" \
@@ -69,16 +64,10 @@ echo "Configuring FFmpeg ..."
 --disable-securetransport \
 --disable-bsfs \
 --disable-filters \
---disable-demuxers \
---enable-demuxer=aac,ape,asf,dsf,flac,mp3,mpc,mpc8,wv,dts,dtshd \
---enable-demuxer=ogg,matroska \
---enable-demuxer=mjpeg,mjpeg_2000,mpjpeg \
+--enable-demuxers \
 --disable-decoders \
---enable-decoder=aac,ape,flac,mp1,mp1_at,mp1float,mp2,mp2_at,mp2float,mpc7,mpc8,dsd_lsbf,dsd_lsbf_planar,dsd_msbf,dsd_msbf_planar,opus,vorbis,wavpack,wmav1,wmav2,wmalossless,wmapro,wmavoice,dca \
---enable-decoder=bmp,png,jpeg2000,jpegls,mjpeg,mjpegb \
+--enable-decoder=8svx_exp,8svx_fib,aac,aac_fixed,aac_at,aac_latm,ac3,ac3_fixed,ac3_at,acelp.kelvin,adpcm_4xm,adpcm_adx,adpcm_afc,adpcm_agm,adpcm_aica,adpcm_argo,adpcm_ct,adpcm_dtk,adpcm_ea,adpcm_ea_maxis_xa,adpcm_ea_r1,adpcm_ea_r2,adpcm_ea_r3,adpcm_ea_xas,g722,g726,g726le,adpcm_ima_alp,adpcm_ima_amv,adpcm_ima_apc,adpcm_ima_apm,adpcm_ima_cunning,adpcm_ima_dat4,adpcm_ima_dk3,adpcm_ima_dk4,adpcm_ima_ea_eacs,adpcm_ima_ea_sead,adpcm_ima_iss,adpcm_ima_mtf,adpcm_ima_oki,adpcm_ima_qt,adpcm_ima_qt_at,adpcm_ima_rad,adpcm_ima_smjpeg,adpcm_ima_ssi,adpcm_ima_wav,adpcm_ima_ws,adpcm_ms,adpcm_mtaf,adpcm_psx,adpcm_sbpro_2,adpcm_sbpro_3,adpcm_sbpro_4,adpcm_swf,adpcm_thp,adpcm_thp_le,adpcm_vima,adpcm_xa,adpcm_yamaha,adpcm_zork,alac,alac_at,amrnb,amr_nb_at,libopencore_amrnb,amrwb,libopencore_amrwb,ape,aptx,aptx_hd,atrac1,atrac3,atrac3al,atrac3plus,atrac3plusal,atrac9,on2avc,binkaudio_dct,binkaudio_rdft,bmv_audio,comfortnoise,cook,derf_dpcm,dolby_e,dsd_lsbf,dsd_lsbf_planar,dsd_msbf,dsd_msbf_planar,dsicinaudio,dss_sp,dst,dca,dvaudio,eac3,eac3_at,evrc,flac,g723_1,g729,gremlin_dpcm,gsm,gsm_ms,gsm_ms_at,hca,hcom,iac,ilbc,ilbc_at,imc,interplay_dpcm,interplayacm,mace3,mace6,metasound,mlp,mp1,mp1float,mp1_at,mp2,mp2float,mp2_at,mp3float,mp3,mp3_at,mp3adufloat,mp3adu,mp3on4float,mp3on4,als,mpc7,mpc8,nellymoser,opus,libopus,paf_audio,pcm_alaw,pcm_alaw_at,pcm_bluray,pcm_dvd,pcm_f16le,pcm_f24le,pcm_f32be,pcm_f32le,pcm_f64be,pcm_f64le,pcm_lxf,pcm_mulaw,pcm_mulaw_at,pcm_s16be,pcm_s16be_planar,pcm_s16le,pcm_s16le_planar,pcm_s24be,pcm_s24daud,pcm_s24le,pcm_s24le_planar,pcm_s32be,pcm_s32le,pcm_s32le_planar,pcm_s64be,pcm_s64le,pcm_s8,pcm_s8_planar,pcm_u16be,pcm_u16le,pcm_u24be,pcm_u24le,pcm_u32be,pcm_u32le,pcm_u8,pcm_vidc,qcelp,qdm2,qdm2_at,qdmc,qdmc_at,real_144,real_288,ralf,roq_dpcm,s302m,sbc,sdx2_dpcm,shorten,sipr,siren,smackaud,sol_dpcm,sonic,libspeex,tak,truehd,truespeech,tta,twinvq,vmdaudio,vorbis,libvorbis,wavesynth,wavpack,ws_snd1,wmalossless,wmapro,wmav1,wmav2,wmavoice,xan_dpcm,xma1,xma2 \
 --disable-parsers \
---enable-parser=aac,flac,mpegaudio,opus,vorbis,dca,ac3 \
---enable-parser=bmp,mjpeg,png \
 --disable-encoders
 
 echo "Done configuring FFmpeg.\n"
